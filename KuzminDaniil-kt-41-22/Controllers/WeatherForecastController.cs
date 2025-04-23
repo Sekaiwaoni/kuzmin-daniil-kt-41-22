7fbecce41ea35ae30f9d3c2c22aacc2d009b2e06
Даниил Кузьмин <dan-kuz201@yandex.ru>
2025-04-23 05:56:46 +0300

    add logging and WeatherForecat test request

--- KuzminDaniil-kt-41-22/Controllers/WeatherForecastController.cs
+++ KuzminDaniil-kt-41-22/Controllers/WeatherForecastController.cs
@@ -18,16 +18,27 @@ namespace KuzminDaniil_kt_41_22.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        //[HttpGet(Name = "GetWeatherForecast")]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
+        [HttpGet(Name = "AddNewSummary")]
+
+        public string[] AddNewSummary(string newSummary)
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            _logger.LogError("New method was called");
+
+            var list = Summaries.ToList();
+            list.Add(newSummary);
+            return list.ToArray();
         }
     }
 }
