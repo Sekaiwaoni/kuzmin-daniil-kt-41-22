d1818b9ad48aae71f93a782675e25f8018c02ac1
Даниил Кузьмин <dan-kuz201@yandex.ru>
2025-04-23 17:31:54 +0300

    added Configurations for Cafedra and Teacher

---
+++ KuzminDaniil-kt-41-22/Database/Configurations/CafedraConfiguration.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Npgsql.EntityFrameworkCore;
+using KuzminDaniil_kt_41_22.Database;
+using KuzminDaniil_kt_41_22.Database.Helper;
+using KuzminDaniil_kt_41_22;
+using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KuzminDaniil_kt_41_22.Database
+{
+    public class CafedraConfiguration : IEntityTypeConfiguration<Cafedra>
+    {
+        private const string TableName = "cd_teacher";
+        
+        public void Configure(EntityTypeBuilder<Cafedra> builder)
+        {
+            //primary key
+            builder
+                .HasKey(p => p.idCafedra)
+                .HasName($"pk_{TableName}_id_Cafedra");
+
+            // Identity generation
+            builder.Property(p => p.idCafedra).ValueGeneratedOnAdd();
+
+            builder.Property(p => p.idCafedra)
+                .HasColumnName("Cafedra_id").HasComment("Идентификатор записи кафедры");
+
+            builder.Property(p => p.ZavCafName)
+                .IsRequired()
+                .HasColumnName("c_ZavCafedra_name")
+                .HasColumnType(ColumnType.String).HasMaxLength(100)
+                .HasComment("ФИО Заведующего кафедрой");
+
+            builder.Property(p => p.NameCafedra)
+                .IsRequired()
+                .HasColumnName("c_Cafedra_name")
+                .HasColumnType(ColumnType.String).HasMaxLength(100)
+                .HasComment("Название кафедры");
+
+            builder.Property(p => p.isZavCaf)
+                .IsRequired()
+                .HasColumnName("c_Cafedra_Заведующий?")
+                .HasColumnType(ColumnType.Bool).HasMaxLength(100)
+                .HasComment("Являеться ли заведующим");
+
+            builder.Property(p => p.idZavCaf)
+                .HasColumnName("idZavCaf")
+                .HasColumnType(ColumnType.Int).HasMaxLength(20)
+                .HasComment("Идентификатор записи заведующего кафедры ");
+            //
+            builder.Property(p => p.FoudationDate)
+                .HasColumnName("FoundationDate")
+                .HasColumnType(ColumnType.Date).HasMaxLength(20)
+                .HasComment("Дата основания");
+            builder.ToTable(TableName)
+                .HasMany("Zavcaf")
+                .WithOne()
+                .HasForeignKey("idZavCaf")
+                .HasConstraintName("fk_f_teacher_id")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.ToTable(TableName)
+                .HasIndex(p => p.idZavCaf, $"idx_{TableName}_fk_f_teacher_id");
+
+            builder.Navigation(p => p.ZavCaf)
+                .AutoInclude();
+        }
+    }
+}
---
+++ KuzminDaniil-kt-41-22/Database/Configurations/TeacherConfiguration.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Npgsql.EntityFrameworkCore;
+using KuzminDaniil_kt_41_22.Database;
+using KuzminDaniil_kt_41_22.Database.Helper;
+using KuzminDaniil_kt_41_22;
+using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KuzminDaniil_kt_41_22.Database
+{
+    public class TeacherConfiguration : IEntityTypeConfiguration<Teacher>
+    {
+        private const string TableName = "cd_teacher";
+        
+        public void Configure(EntityTypeBuilder<Teacher> builder)
+        {
+            //primary key
+            builder
+                .HasKey(p => p.idTeacher)
+                .HasName($"pk_{TableName}_id_Teacher");
+
+            // Identity generation
+            builder.Property(p => p.idTeacher).ValueGeneratedOnAdd();
+
+            builder.Property(p => p.idTeacher)
+                .HasColumnName("Teacher_id").HasComment("Идентификатор записи преподавателя");
+
+            builder.Property(p => p.SurnameTeacher)
+                .IsRequired()
+                .HasColumnName("c_Teacher_surname")
+                .HasColumnType(ColumnType.String).HasMaxLength(100)
+                .HasComment("Фамилия преподавателя");
+
+            builder.Property(p => p.NameTeacher)
+                .IsRequired()
+                .HasColumnName("c_Teacher_name")
+                .HasColumnType(ColumnType.String).HasMaxLength(100)
+                .HasComment("Имя преподавателя");
+
+            builder.Property(p => p.SecsurnameTeacher)
+                .IsRequired()
+                .HasColumnName("c_Teacher_secsurname")
+                .HasColumnType(ColumnType.String).HasMaxLength(100)
+                .HasComment("Отчество преподавателя");
+
+            builder.Property(p => p.idCafedra)
+                .HasColumnName("Cafedra_id")
+                .HasColumnType(ColumnType.Int).HasMaxLength(20)
+                .HasComment("Идентификатор записи кафедры преподавателя");
+
+            builder.ToTable(TableName)
+                .HasOne(p => p.Cafedra)
+                .WithMany()
+                .HasForeignKey(p => p.idCafedra)
+                .HasConstraintName("fk_f_groop_id")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.ToTable(TableName)
+                .HasIndex(p => p.idCafedra, $"idx_{TableName}_fk_f_cafedra_id");
+
+            builder.Navigation(p => p.Cafedra)
+                .AutoInclude();
+        }
+    }
+}
